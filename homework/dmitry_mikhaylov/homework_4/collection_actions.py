0bc2fba453c0054fbb8e1876bb453d15c6188cd9
Дмитрий Михайлов <d.y.mikhaylov@yandex.ru>
2024-12-13 11:30:13 +0200

    corrected code

--- homework/dmitry_mikhaylov/homework_4/collection_actions.py
+++ homework/dmitry_mikhaylov/homework_4/collection_actions.py
@@ -1,27 +1,20 @@
 my_dict = {}
 
-for collection_type in ['tuple', 'list', 'dict', 'set']:
-    if collection_type == 'tuple':
-        my_dict[collection_type] = (1, 2, 'three', -4, 5.0, 'last')
-    if collection_type == 'list':
-        my_dict[collection_type] = [1, 2, "three", -4, 5.0, 'not_last', 1]
-    if collection_type == 'dict':
-        my_dict[collection_type] = {'first': 1, 'second': 2, 3: 'three',
-                                    'negative': -4, 'float': 5.0, -1: 'last'}
-    if collection_type == 'set':
-        my_dict[collection_type] = set(my_dict['list'])
+my_dict['tuple'] = (1, 2, 'three', -4, 5.0, 'last')
+my_dict['list'] = [1, 2, "three", -4, 5.0, 'not_last', 1]
+my_dict['dict'] = {'first': 1, 'second': 2, 3: 'three',
+                   'negative': -4, 'float': 5.0, -1: 'last'}
+my_dict['set'] = set(my_dict['list'])
 
-for collection_type in my_dict.keys():
-    if collection_type == 'tuple':
-        print(my_dict[collection_type][-1])
-    if collection_type == 'list':
-        my_dict[collection_type].append('very_last')
-        my_dict[collection_type].pop(1)
-    if collection_type == 'dict':
-        my_dict[collection_type]['42'] = 42
-        del my_dict[collection_type]['second']
-    if collection_type == 'set':
-        my_dict[collection_type].add(999999)
-        my_dict[collection_type].remove(1)
+print(my_dict['tuple'][-1])
+
+my_dict['list'].append('very_last')
+my_dict['list'].pop(1)
+
+my_dict['dict']['i am a tuple'] = 42
+del my_dict['dict']['second']
+
+my_dict['set'].add(999999)
+my_dict['set'].remove(1)
 
 print(my_dict)
